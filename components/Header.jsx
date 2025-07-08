0109ea8944b9a386f6a91d008e2560b9a01e2147
Your Name <shwetank00@gmail.com>
2025-07-09 05:05:27 +0530

    created database models, storing user data in database, initialising prisma, syncing database to the environment

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,9 +9,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
 // components/Header.jsx
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     //! LOGO
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b ">
